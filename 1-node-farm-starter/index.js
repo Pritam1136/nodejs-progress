594359c7623985cbc7eb73d2b4c614535c93e143
Pritam Roy <pritamroy1136@gmail.com>
2023-05-31 13:54:56 +0530

    used slugify

--- 1-node-farm-starter/index.js
+++ 1-node-farm-starter/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require(`./modules/replacemodule`);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
@@ -18,6 +19,8 @@ const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   "utf-8"
 );
+// const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+// console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
