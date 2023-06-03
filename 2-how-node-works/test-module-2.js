a39a3ddeea029d91f3ba51e4013c6b996182fd61
Pritam Roy <pritamroy1136@gmail.com>
2023-06-03 09:13:46 +0530

    modules in practice

---
+++ 2-how-node-works/test-module-2.js
@@ -0,0 +1,15 @@
+exports.add = (a, b) => {
+  return a + b;
+};
+
+exports.multiply = (a, b) => {
+  return a * b;
+};
+
+exports.minus = (a, b) => {
+  return a - b;
+};
+
+exports.division = (a, b) => {
+  return a / b;
+};
---
+++ 2-how-node-works/test-module-3.js
@@ -0,0 +1,5 @@
+console.log("Hello world");
+
+module.exports = () => {
+  console.log("Log this beautiful text 😍");
+};
