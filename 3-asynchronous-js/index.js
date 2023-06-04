42a06f41d07715123314c0f9d4bdef8a98574939
Pritam Roy <pritamroy1136@gmail.com>
2023-06-04 13:15:54 +0530

    Building promises

--- 3-asynchronous-js/index.js
+++ 3-asynchronous-js/index.js
@@ -1,23 +1,45 @@
 const fs = require("fs");
+const { resolve } = require("path");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, "utf-8", (err, data) => {
-  //   console.log(data);
-
-  superagent
-    .get(
-      `https://dog.ceo/api/breed/${data}/images/random
-  `
-    )
-    .end((err, res) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(res.body.message);
-
-      fs.writeFile("dog-img.txt", res.body.message, (err) => {
-        if (err) console.log(err);
-      });
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, "utf-8", (err, data) => {
+      if (err) reject("Could not find file. 😒😒😒");
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) {
+        reject("could not find.");
+      }
+      resolve("Success !!!!!!");
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    //   console.log(data);
+
+    return superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random
+`
+    );
+  })
+  .then((res) => {
+    // console.log(res.body.message);
+    return writeFilePro("dog-img.txt", res.body.message);
+  })
+  .then(() => {
+    console.log("Random dog image saved to file !");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// fs.readFile(`${__dirname}/dog.txt`, "utf-8", (err, data) => {});
