79b769833e3a3572a722c31461e7ac29a6b17279
Pritam Roy <pritamroy1136@gmail.com>
2023-06-05 21:31:26 +0530

    Starting with express

---
+++ 4-natours/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'hello from the server side' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post on this endpoint................');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
