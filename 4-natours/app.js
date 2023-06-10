3a062dfb44216b2d29f2eee6428a0758a1b51481
Pritam Roy <pritamroy1136@gmail.com>
2023-06-10 12:39:28 +0530

    serving static files

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -9,8 +9,9 @@ const app = express();
 // Middleware
 
 app.use(morgan('dev'));
-
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middle ware 😊');
